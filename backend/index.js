9337ce566ea7c51fafdd8a95d7a47808d917095c
Vince Nguyen <vincehnguyen@ucsb.edu>
2023-02-03 14:41:26 -0800

    Basic node server

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,22 +15,22 @@ function App() {
     const app = new PIXI.Application({
       view: document.getElementById("canvas"),
       autoStart: true,
-      resizeTo: window
+      resizeTo: window,
+      backgroundColor: 0x333333
     });
 
     Live2DModel.from("resources/runtimeb/mao_pro_t02.model3.json", {
       idleMotionGroup: "Idle"
     }).then((model) => {
-      console.log(model)
       app.stage.addChild(model);
       model.anchor.set(0.5, 0.5);
       model.position.set(window.innerWidth / 4, window.innerHeight / 4);
       model.scale.set(0.09, 0.09);
 
       function resizeWaifu() {
-        model.scale.set(window.innerHeight / 1280 * .8);
+        model.scale.set(window.innerHeight / 1280 * .5);
         model.x = (window.innerWidth) / 2;
-        model.y = (window.innerHeight + 2000) / 2;
+        model.y = (window.innerHeight + 1200) / 2;
       }
       resizeWaifu();
 
